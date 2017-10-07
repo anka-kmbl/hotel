7a10aed59f184faaafa136e67312203e7eaa3374
anka-kmbl <anna.maksymenko@nure.ua>
2017-10-07 20:58:27 +0300

    a lot of changes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,38 +3,235 @@
 //   document.getElementsByClassName('nav')[0].style.backgroundColor = '#937B71';
 // })
 window.onscroll = function() {
-  document.getElementsByClassName('nav')[0].style.backgroundColor = '#888485';
-}
+	document.getElementsByClassName('nav')[0].style.backgroundColor = '#888485';
+};
+window.onload = function() {
+	document.getElementsByClassName('step')[0].style.border = 'solid 3px #FD6290';
+};
+
+document.getElementById('goBackStart').onclick = goBack;
 
 document.getElementById('dataForm').addEventListener('submit', (e) => {
-  e.preventDefault();
-  getNewData()
-    .then((result) => {
-      document.getElementsByClassName('container')[0].innerHTML = result;
-      document.getElementsByClassName('step')[1].style.borderColor = '#FD6290';
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-})
+	e.preventDefault();
+	getNewData()
+		.then((result) => {
+			let rooms = result;
+			// displayRoomsCont();
+			display(1, 'hotelsPage');
+			displayRooms(rooms);
+			// document.getElementsByClassName('container')[0].innerHTML = result;
+			// document.getElementById('formData').classList.add('displayNone');
+			// document.getElementsByClassName('step')[1].style.borderColor = '#FD6290';
+			// document.getElementsByClassName('step')[1].style.border = 'solid 3px #FD6290';
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
 
+document.getElementById('filterForm').addEventListener('submit', (e) => {
+	e.preventDefault();
+	let form = document.getElementById('filterForm');
+	let inputs = form.getElementsByTagName('input');
+	let formData = {};
+	for(let i = 0; i < inputs.length - 1; i++) {
+		if(inputs[i].checked) {
+			if(inputs[i].name == 'budget') {
+				switch(inputs[i].id) {
+				case 'budget1':
+					formData.price = 110;
+					break;
+				case 'budget2':
+					formData.price = 150;
+					break;
+				case 'budget3':
+					formData.price = 200;
+					break;
+				case 'budget4':
+					formData.price = 201;
+					break;
+				}
+			} if(inputs[i].name == 'meal') {
+				formData.meal = inputs[i].value;
+				
+			}	else {
+				formData[inputs[i].id] = inputs[i].value;
+			}
+		} 
+
+		// if(inputs[i].type == 'text') {
+		// 	formData[inputs[i].id] = inputs[i].value;
+		// }
+	}
+	console.log(formData);
+	filter(formData)
+		.then((res) => {
+			// let rooms = JSON.parse(res);
+			console.log(res);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
+// function displayStart() {
+	
+// }
+function filter(data) {
+	return new Promise((res,rej) => {
+		let xhr = new XMLHttpRequest();
+		xhr.open('POST', 'api/pickRoom/filter');
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.send(JSON.stringify(data));
+		xhr.addEventListener('load', (e) => {
+			let xhttp = e.target;
+			if(xhttp.status !== 200) {
+				rej(xhttp.response);
+			}
+			res(JSON.parse(xhttp.response));
+		});
+
+	});
+}
+function goBack() {
+	display(0, 'formDiv');
+	
+}
+function display(step, pageToSee) {
+	document.getElementsByClassName('formDiv')[0].classList.add('displayNone');
+	document.getElementsByClassName('hotelsPage')[0].classList.add('displayNone');
+	// document.getElementsByClassName('payment')[0].classList.add('displayNone');
+	for(let i = 0; i < document.getElementsByClassName('step').length; i++) {
+		document.getElementsByClassName('step')[i].style.border = ' 1px solid #937B71';
+	}
+	
+	document.getElementsByClassName(pageToSee)[0].classList.remove('displayNone');
+	document.getElementsByClassName('step')[step].style.border = 'solid 3px #FD6290';
+	// document.getElementsByClassName('hotelsPage')[0].classList.toggle('displayNone');
+	
+}
+function displayRooms(roomsObj) {
+	
+	let hotelsCont = document.getElementsByClassName('hotelsContainer')[0];
+	
+	hotelsCont.innerHTML = '';
+	if(roomsObj === 0){
+		let errorText = document.createElement('p');
+		errorText.innerHTML = 'К сожалению, в номерах может разместиться не более 4 человек.<br/>' +
+		'Предлагаем забронировать два подходящих номера отдельно.<br/> '+
+		'Вы можете вернуться и ввести другое количество людей, нажав на кнопку ниже.<br/>';
+		let backButton = document.createElement('button');
+		backButton.onclick = goBack;
+		backButton.innerHTML = 'Указать другое количество';
+		hotelsCont.appendChild(errorText);
+		hotelsCont.appendChild(backButton);
+		
+
+	}
+	
+	for(let key in roomsObj) {
+		let div = document.createElement('div');
+		let divPhoto = document.createElement('div');
+		let name = document.createElement('p');
+		name.innerHTML = roomsObj[key].name;
+		let img = document.createElement('img');
+		img.src = roomsObj[key].photo;
+		let info = document.createElement('ul');
+
+		let price = document.createElement('li');
+		price.innerHTML = roomsObj[key].price + '$';
+		// info.appendChild(name);
+		info.appendChild(price);
+		let wifi = document.createElement('li');
+		let meal = document.createElement('li');
+		let bathroom = document.createElement('li');
+		let balcony =document.createElement('li');
+		let kitchen =document.createElement('li');
+		console.log(roomsObj[key].facilities);
+		if(roomsObj[key].facilities.wifi) {
+			wifi.innerHTML = 'бесплатный Wi-Fi';
+			info.appendChild(wifi);
+		}
+		if(roomsObj[key].facilities.bathroom) {
+			bathroom.innerHTML = 'ванная комната и туалет';
+			info.appendChild(bathroom);
+		}
+		if(roomsObj[key].facilities.kitchen) {
+			kitchen.innerHTML = 'мини-кухня';
+			info.appendChild(kitchen);
+		}
+		if(roomsObj[key].facilities.balcony) {
+			balcony.innerHTML = 'балкон';
+			info.appendChild(balcony);
+		}
+
+		switch(roomsObj[key].meal) {
+		case 'breakfast':
+			meal.innerHTML = 'Завтрак включен';
+			info.appendChild(meal);
+			break;
+		case 'breakfastNdinner':
+			meal.innerHTML = 'Завтрак и ужин';
+			info.appendChild(meal);
+			break;
+		case 'all':
+			meal.innerHTML = 'Полный пансион';
+			info.appendChild(meal);
+			break;
+		case '':
+			meal.innerHTML = 'Питание не входит в стоимость';
+			info.appendChild(meal);
+			break;
+		}
+
+		// div.appendChild(name);
+		// div.appendChild(img);
+		divPhoto.appendChild(name);
+		divPhoto.appendChild(img);
+		div.appendChild(divPhoto);
+		div.appendChild(info);
+		divPhoto.classList.add('roomsPhotoDiv');
+		div.classList.add('roomsInfoDiv');
+		info.classList.add('roomsInfoList');
+		hotelsCont.appendChild(div);
+
+	}
+}
 function getNewData() {
-  let  = document.getElementsByName('lastName')[0];
-
-  return new Promise((res, rej) => {
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/api/pickRoom');
-    xhr.send({
-      peopleNum: 
-    });
-    xhr.addEventListener('load', (e) => {
-      let xhttp = e.target;
-      if(xhttp.status !== 200) {
-        rej(xhttp.response);
-      }
-      let result = JSON.parse(xhttp.response);
-      res(result.resp);
-    })
-  })
+	let firstName = document.getElementsByName('firstName')[0].value;
+	let lastName = document.getElementsByName('lastName')[0].value;
+	let patr = document.getElementsByName('patr')[0].value;
+	let checkIn = document.getElementsByName('checkIn')[0].value;
+	let checkOut = document.getElementsByName('checkOut')[0].value;
+	let numAdultsSelect = document.getElementsByName('numAdults')[0];
+	let numAdults = +numAdultsSelect.options[numAdultsSelect.selectedIndex].value;
+	let numChildrenSelect = document.getElementsByName('numChildren')[0];
+	let numChildren = +numChildrenSelect.options[numChildrenSelect.selectedIndex].value;
+	let peopleNum = numAdults + numChildren;
+	
+	return new Promise((res, rej) => {
+		let xhr = new XMLHttpRequest();
+		xhr.open('POST', '/api/pickRoom');
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		
+		xhr.send(JSON.stringify({
+			peopleNum: +peopleNum,
+			firstName: firstName,
+			lastName: lastName,
+			patr: patr,
+			checkIn: checkIn,
+			checkOut: checkOut
+		}));
+		xhr.addEventListener('load', (e) => {
+			let xhttp = e.target;
+			if(xhttp.status !== 200) {
+				rej(xhttp.response);
+			}
+			
+			let result = JSON.parse(xhttp.response);
+			console.log(result);
+			res(result);
+		});
+	});
 }
 // function setNewData()
--- routes/roomsRoutes.js
+++ routes/roomsRoutes.js
@@ -1,163 +1,254 @@
-'use strict'
+'use strict';
 const express = require('express');
 let router = express.Router();
-let roomsStorage = require('../rooms');
+// let roomsStorage = require('../rooms');
 let roomsObj = {
-  singleStandart:{
-    name: 'Одноместный стандарт',
-    price: 80,
-    peopleNum: 1,
-    roomFacilities: {
-      wifi: true
-    },
+	singleStandart:{
+		name: 'Одноместный стандарт',
+		price: 80,
+		peopleNum: 1,
+		facilities: {
+			wifi: true
+		},
+		photo: '/images/single-room-1.jpg',
+		meal: ''
      
 
-  },
-  singleStandartBetter:{
-    name: 'Одноместный стандарт улучшенный',
-    price: 85,
-    peopleNum: 1,
-    facilities: {
-      wifi: true,
-      balcony: true,
-    },
-    meal: 'breakfast'
+	},
+	singleStandartBetter:{
+		name: 'Одноместный стандарт улучшенный',
+		price: 85,
+		peopleNum: 1,
+		facilities: {
+			wifi: true,
+			balcony: true,
+		},
+		meal: 'breakfast',
+		photo: '/images/single-room-2.jpg'
 
-  },
-  singleLux:{
-    name: 'Одноместный люкс',
-    price: 95,
-    peopleNum: 1,
-    facilities: {
-      wifi: true,
-      balcony: true,
-      kitchen: true,
-      bathroom: true
-    }, 
-    meal: 'beakfastNdinner'
+	},
+	singleLux:{
+		name: 'Одноместный люкс',
+		price: 95,
+		peopleNum: 1,
+		facilities: {
+			wifi: true,
+			balcony: true,
+			kitchen: true,
+			bathroom: true
+		}, 
+		meal: 'breakfastNdinner',
+		photo: '/images/single-room-4.jpg'
     
 
-  },
-  doubleEconom:{
-    name: 'Стандарт двуместный',
-    price: 100,
-    peopleNum: 2,
-    facilities: {
-      wifi: true
-    }
+	},
+	doubleEconom:{
+		name: 'Стандарт двуместный',
+		price: 100,
+		peopleNum: 2,
+		facilities: {
+			wifi: true
+		},
+		meal: '',
+		photo: '/images/double-1.jpg'
     
     
 
-  },
-  doubleStandart :{
-    name: 'Стандарт двуместный',
-    price: 125,
-    peopleNum: 2,
-    facilities: {
-      wifi: true,
-      bathroom: true
-    }, 
-    meal: 'beakfast'
+	},
+	doubleStandart :{
+		name: 'Стандарт двуместный',
+		price: 125,
+		peopleNum: 2,
+		facilities: {
+			wifi: true,
+			bathroom: true
+		}, 
+		meal: 'breakfast',
+		photo: '/images/double-2.jpg'
     
 
-  },
-  doubleStandartBetter :{
-    name: 'Стандарт двуместный улучшенный',
-    price: 145,
-    peopleNum: 2,
-    facilities: {
-      wifi: true,
-      bathroom: true,
-      kitchen: true
-    }, 
-    meal: 'breakfastNDinner'
+	},
+	doubleStandartBetter :{
+		name: 'Стандарт двуместный улучшенный',
+		price: 145,
+		peopleNum: 2,
+		facilities: {
+			wifi: true,
+			bathroom: true,
+			kitchen: true
+		}, 
+		meal: 'breakfastNDinner',
+		photo: '/images/double-3.jpg'
     
 
-  },
-  doubleLux :{
-    name: 'Двуместный люкс',
-    price: 170,
-    peopleNum: 2,
-    facilities: {
-      wifi: true,
-      bathroom: true,
-      kitchen: true,
-      balcony: true,
-    }, 
-    meal: 'all'
+	},
+	doubleLux :{
+		name: 'Двуместный люкс',
+		price: 170,
+		peopleNum: 2,
+		facilities: {
+			wifi: true,
+			bathroom: true,
+			kitchen: true,
+			balcony: true,
+		}, 
+		meal: 'all',
+		photo: '/images/double-4.jpg'
     
 
-  },
-  tripleEconom :{
-    name: 'Трехместный эконом',
-    price: 120,
-    peopleNum: 3,
-    facilities: {
-      wifi: true
-    }, 
-    meal: 'no'
+	},
+	tripleEconom :{
+		name: 'Трехместный эконом',
+		price: 120,
+		peopleNum: 3,
+		facilities: {
+			wifi: true
+		}, 
+		meal: '',
+		photo: '/images/triple-1.jpg'
     
 
-  },
-  tripleStandart :{
-    name: 'Трехместный стандарт',
-    price: 170,
-    peopleNum: 3,
-    facilities: {
-      wifi: true,
-      bathroom: true
-    }, 
-    meal: 'breakfast'
+	},
+	tripleStandart :{
+		name: 'Трехместный стандарт',
+		price: 170,
+		peopleNum: 3,
+		facilities: {
+			wifi: true,
+			bathroom: true
+		}, 
+		meal: 'breakfast',
+		photo: '/images/triple-2.jpg'
     
 
-  },
-  tripleLux:{
-    name: 'Трехместный люкс',
-    price: 220,
-    peopleNum: 3,
-    facilities: {
-      wifi: true,
-      bathroom: true,
-      kitchen: true,
-      balcony: true
-    }, 
-    meal: 'all'
+	},
+	tripleLux:{
+		name: 'Трехместный люкс',
+		price: 220,
+		peopleNum: 3,
+		facilities: {
+			wifi: true,
+			bathroom: true,
+			kitchen: true,
+			balcony: true
+		}, 
+		meal: 'all',
+		photo: '/images/triple-3.jpg'
     
 
-  },
-  fourStandart:{
-    name: 'Четырехместный стандарт',
-    price: 230,
-    peopleNum: 4,
-    facilities: {
-      wifi: true,
-      bathroom: true
-    }, 
-    meal: 'breakfast'
+	},
+	fourStandart:{
+		name: 'Четырехместный стандарт',
+		price: 230,
+		peopleNum: 4,
+		facilities: {
+			wifi: true,
+			bathroom: true
+		}, 
+		meal: 'breakfast',
+		photo: '/images/four-1.jpg'
     
 
-  },
-  fourLux:{
-    name: 'Четырехместный стандарт',
-    price: 300,
-    peopleNum: 4,
-    facilities: {
-      wifi: true,
-      bathroom: true,
-      kitchen: true,
-      balcony: true
-    }, 
-    meal: 'all'
+	},
+	fourLux:{
+		name: 'Четырехместный люкс',
+		price: 300,
+		peopleNum: 4,
+		facilities: {
+			wifi: true,
+			bathroom: true,
+			kitchen: true,
+			balcony: true
+		}, 
+		meal: 'all',
+		photo: '/images/four-2.jpg'
     
 
-  }
-}
-router.get('/', (req, res) => {
-
-  // res.send(JSON.stringify({resp: true}));
-  // res.send('ok');
+	}
+};
+let apprRooms;
+let budget = {
+	110: 0,
+	150: 110,
+	200: 150,
+	200: 10000
+};
+router.post('/', (req, res) => {
+	console.log(req.body);
+	
+	apprRooms = findApprRooms(req.body);
+	console.log(apprRooms);
+	res.send(JSON.stringify(apprRooms));
+	// res.send(JSON.stringify({resp: true}));
+	// res.send('ok');
   
-})
+});
 
+router.post('/filter', (req, res) => {
+	console.log(req.body);
+	let filters = req.body;
+	let filteredRooms = findFilterRooms(filters);
+	console.log(filteredRooms);
+	res.json(filteredRooms);
+});
+
+function findFilterRooms(filters) {
+	// for(let key in filters) {
+	// 	for(let i in apprRooms) {
+	// 		if(filters)
+	// 		for(let k in facilities) {
+	// 			if(k == key) {
+
+	// 			}
+	// 		}
+	// 		if(apprRooms[i].facilities ==)
+	// 	}
+	// }
+	let max = filters.price;
+	let min = budget[filters.price];
+	let filteredRooms = {};
+	let count = 0;
+	let countFilters = 0;
+	console.log('in filter');
+	for(let j in apprRooms) {
+		if(apprRooms[j].price >= min && apprRooms[j].price <= max) {
+			console.log(1);
+			if(apprRooms[j].meal == filters.meal) {
+				console.log(2);
+				
+				for(let key in apprRooms.facilities) {
+					for(let i in filters) {
+						countFilters++;
+						if(i == key) {
+							count++;
+						}
+					}
+						
+				}
+				console.log(count);
+				console.log(countFilters);
+				if(countFilters - 1 <= count) {
+					filteredRooms[j] = apprRooms[j];
+				}
+			}
+		}
+	}
+	return filteredRooms;
+	
+}
+function findApprRooms(options) {
+	let rooms = {};
+	let check = false;
+	for(let key in roomsObj) {
+		if(roomsObj[key].peopleNum === options.peopleNum) {
+			rooms[key] = roomsObj[key];
+			check = true;
+		}
+	}
+	if(!check) {
+		return 0;
+	}
+	return rooms;
+}
 
 module.exports = router;
